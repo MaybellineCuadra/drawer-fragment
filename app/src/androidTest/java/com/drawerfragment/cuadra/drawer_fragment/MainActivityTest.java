0ff177017be00229eb301c1cc8cc9ef7e080e192
Maybelline Cuadra <cdrmaybelline@gmail.com>
2018-10-13 17:36:35 +0800

    Resolve Conflict

---
+++ app/src/androidTest/java/com/drawerfragment/cuadra/drawer_fragment/MainActivityTest.java
@@ -0,0 +1,5 @@
+package com.drawerfragment.cuadra.drawer_fragment;
+
+public class MainActivityTest {
+
+}
---
+++ app/src/main/java/com/drawerfragment/cuadra/drawer_fragment/TextFieldDemo.java
@@ -0,0 +1,24 @@
+package com.drawerfragment.cuadra.drawer_fragment;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFieldDemo extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.textfield_demo, container, false);
+
+        return view;
+
+    }
+}
+
